7de53aee7fb5dcfe85273d2b671f6ce1a44021d2
Vladislav Selitskii <selitskiivd@haulmont.com>
2021-07-24 20:29:19 +0400

    Fixes #8

--- src/main/java/org/example/controller/OrderController.java
+++ src/main/java/org/example/controller/OrderController.java
@@ -5,31 +5,27 @@ import org.example.dto.OrderRequestDto;
 import org.example.dto.OrderStatusRequestDto;
 import org.example.dto.OrderStatusResponseDto;
 import org.example.service.OrderService;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/rest")
 @AllArgsConstructor
 public class OrderController {
     private OrderService orderService;
 
     @PostMapping("/register.do")
-    @ResponseBody
     public void register(OrderRequestDto orderRequestDto) {
         orderService.register(orderRequestDto);
     }
 
     @PostMapping("/reverse.do")
-    @ResponseBody
     public OrderStatusResponseDto cancel(OrderStatusRequestDto orderStatusRequestDto) {
         return orderService.cancel(orderStatusRequestDto);
     }
 
     @PostMapping("/getOrderStatus.do")
-    @ResponseBody
     public OrderStatusResponseDto getOrderStatus(OrderStatusRequestDto orderStatusRequestDto) {
         return orderService.getOrderStatus(orderStatusRequestDto);
     }
